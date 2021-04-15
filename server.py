4994223b6d40a68ec495dfe764ef03fda056a276
ROU001 <79861981+ROU001@users.noreply.github.com>
2021-04-14 17:30:55 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,32 +1,33 @@
 import time, socket, sys
- 
-new_socket = socket.socket()
+print('Setup Server...')
+time.sleep(1)
+#Get the hostname, IP Address from socket and set Port
+soc = socket.socket()
 host_name = socket.gethostname()
-s_ip = socket.gethostbyname(host_name)
- 
-port = 8080
- 
-new_socket.bind((host_name, port))
-print( "Binding successful!”)
-print("This is your IP: ", s_ip)
- 
+ip = socket.gethostbyname(host_name)
+port = 1234
+soc.bind((host_name, port))
+print(host_name, '({})'.format(ip))
 name = input('Enter name: ')
- 
-new_socket.listen(1) 
- 
- 
-conn, add = new_socket.accept()
- 
-print("Received connection from ", add[0])
-print('Connection Established. Connected From: ',add[0])
- 
-client = (conn.recv(1024)).decode()
-print(client + ' has connected.')
- 
-conn.send(name.encode())
-while True:
-    message = input('Me : ')
-    conn.send(message.encode())
-    message = conn.recv(1024)
-    message = message.decode()
-    print(client, ':', message)
+soc.listen(1) #Try to locate using socket
+print('Waiting for incoming connections...')
+connection, addr = soc.accept()
+print("Received connection from ", addr[0], "(", addr[1], ")\n")
+print('Connection Established. Connected From: {}, ({})'.format(addr[0], addr[0]))
+#get a connection from client side
+client_name = connection.recv(1024)
+client_name = client_name.decode()
+print(client_name + ' has connected.')
+print('Press [bye] to leave the chat room')
+connection.send(name.encode())
+whileTrue:
+message = input('Me > ')
+if message == '[bye]':
+message = 'Good Night...'
+connection.send(message.encode())
+print("\n")
+break
+connection.send(message.encode())
+message = connection.recv(1024)
+message = message.decode()
+print(client_name, '>', message)
