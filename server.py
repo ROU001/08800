283a3af063eee956678f609d33e82879ab3ed493
ROU001 <79861981+ROU001@users.noreply.github.com>
2021-04-14 17:36:44 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,33 +1,30 @@
-import time, socket, sys
-print('Setup Server...')
-time.sleep(1)
-#Get the hostname, IP Address from socket and set Port
-soc = socket.socket()
-host_name = socket.gethostname()
-ip = socket.gethostbyname(host_name)
-port = 1234
-soc.bind((host_name, port))
-print(host_name, '({})'.format(ip))
-name = input('Enter name: ')
-soc.listen(1) #Try to locate using socket
-print('Waiting for incoming connections...')
-connection, addr = soc.accept()
-print("Received connection from ", addr[0], "(", addr[1], ")\n")
-print('Connection Established. Connected From: {}, ({})'.format(addr[0], addr[0]))
-#get a connection from client side
-client_name = connection.recv(1024)
-client_name = client_name.decode()
-print(client_name + ' has connected.')
-print('Press [bye] to leave the chat room')
-connection.send(name.encode())
-whileTrue:
-message = input('Me > ')
-if message == '[bye]':
-message = 'Good Night...'
-connection.send(message.encode())
-print("\n")
-break
-connection.send(message.encode())
-message = connection.recv(1024)
-message = message.decode()
-print(client_name, '>', message)
+import socket,select
+
+port = 12345
+socket_list = []
+users = {}
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind(('',port))
+server_socket.listen(5)
+socket_list.append(server_socket)
+while True:
+    ready_to_read,ready_to_write,in_error = select.select(socket_list,[],[],0)
+    for sock in ready_to_read:
+        if sock == server_socket:
+            connect, addr = server_socket.accept()
+            socket_list.append(connect)
+            connect.send("You are connected from:" + str(addr))
+        else:
+            try:
+                data = sock.recv(2048)
+                if data.startswith("#"):
+                    users[data[1:].lower()]=connect
+                    print "User " + data[1:] +" added."
+                    connect.send("Your user detail saved as : "+str(data[1:]))
+                elif data.startswith("@"):
+                    users[data[1:data.index(':')].lower()].send(data[data.index(':')+1:])
+            except:
+                continue
+
+server_socket.close()
