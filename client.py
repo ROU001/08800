9c151d9f38322da86ee4ae71c8f5ac360f17e133
ROU001 <79861981+ROU001@users.noreply.github.com>
2021-04-14 17:31:43 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,24 +1,32 @@
 import time, socket, sys
- 
-socket_server = socket.socket()
-server_host = socket.gethostname()
-ip = socket.gethostbyname(server_host)
-sport = 8080
- 
-print('This is your IP address: ',ip)
-server_host = input('Enter friend\'s IP address:')
-name = input('Enter Friend\'s name: ')
- 
- 
-socket_server.connect((server_host, sport))
- 
-socket_server.send(name.encode())
-server_name = socket_server.recv(1024)
+print('Client Server...')
+time.sleep(1)
+#Get the hostname, IP Address from socket and set Port
+soc = socket.socket()
+shost = socket.gethostname()
+ip = socket.gethostbyname(shost)
+#get information to connect with the server
+print(shost, '({})'.format(ip))
+server_host = input('Enter server\'s IP address:')
+name = input('Enter Client\'s name: ')
+port = 1234
+print('Trying to connect to the server: {}, ({})'.format(server_host, port))
+time.sleep(1)
+soc.connect((server_host, port))
+print("Connected...\n")
+soc.send(name.encode())
+server_name = soc.recv(1024)
 server_name = server_name.decode()
- 
-print(server_name,' has joined...')
-while True:
-    message = (socket_server.recv(1024)).decode()
-    print(server_name, ":", message)
-    message = input("Me : ")
-    socket_server.send(message.encode())
+print('{} has joined...'.format(server_name))
+print('Enter [bye] to exit.')
+whileTrue:
+message = soc.recv(1024)
+message = message.decode()
+print(server_name, ">", message)
+message = input(str("Me > "))
+if message == "[bye]":
+message = "Leaving the Chat room"
+soc.send(message.encode())
+print("\n")
+break
+soc.send(message.encode())
