144ae089ecbc39e6405a718394d033ed244d5566
ROU001 <79861981+ROU001@users.noreply.github.com>
2021-04-14 17:37:37 -0700

    Update client.py

--- client.py
+++ client.py
@@ -1,32 +1,27 @@
-import time, socket, sys
-print('Client Server...')
-time.sleep(1)
-#Get the hostname, IP Address from socket and set Port
-soc = socket.socket()
-shost = socket.gethostname()
-ip = socket.gethostbyname(shost)
-#get information to connect with the server
-print(shost, '({})'.format(ip))
-server_host = input('Enter server\'s IP address:')
-name = input('Enter Client\'s name: ')
-port = 1234
-print('Trying to connect to the server: {}, ({})'.format(server_host, port))
-time.sleep(1)
-soc.connect((server_host, port))
-print("Connected...\n")
-soc.send(name.encode())
-server_name = soc.recv(1024)
-server_name = server_name.decode()
-print('{} has joined...'.format(server_name))
-print('Enter [bye] to exit.')
-whileTrue:
-message = soc.recv(1024)
-message = message.decode()
-print(server_name, ">", message)
-message = input(str("Me > "))
-if message == "[bye]":
-message = "Leaving the Chat room"
-soc.send(message.encode())
-print("\n")
-break
-soc.send(message.encode())
+import socket
+
+client_socket = socket.socket()
+port = 8080
+client_socket.connect(('127.0.0.1',port))
+
+#recieve connection message from server
+recv_msg = client_socket.recv(1024)
+print recv_msg
+
+#send user details to server
+send_msg = raw_input("Enter your user name(prefix with #):")
+client_socket.send(send_msg)
+
+
+#receive and send message from/to different user/s
+
+while True:
+    recv_msg = client_socket.recv(1024)
+    print recv_msg
+    send_msg = raw_input("Send your message in format [@user:message] ")
+    if send_msg == 'exit':
+        break;
+    else:
+        client_socket.send(send_msg)
+
+client_socket.close()
